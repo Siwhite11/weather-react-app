b1a71c84e581c3590d404abfb2cf8be73b27d1a0
Stephanie White <stephanieiwhite11@gmail.com>
2020-08-23 14:00:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function Container() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1>New York</h1>
+
+      <div className="row">
+        <div className="col">
+          <h2>
+            <span id="tempNow" />
+            <a href="#" id="f-link" className="active">
+              75 °F{" "}
+            </a>{" "}
+            |{" "}
+            <a href="#" id="c-link">
+              {" "}
+              °C{" "}
+            </a>
+          </h2>
+          <h2>
+            {" "}
+            <span id="description" />
+          </h2>
+          <img id="icon" className="now" src="" alt="" />
+          <div class="col">
+            <span class="info" />
+
+            <form>
+              <input
+                className="search2"
+                id="location"
+                type="submit"
+                value="Current Location"
+              />
+            </form>
+            <h4>Mon August 3st 2020</h4>
+            <br />
+            <ul>
+              <Weather />
+            </ul>
+            <br />
+
+            <div class="col">
+              <small class="me">
+                {" "}
+                Open Source code by{" "}
+                <a
+                  href="https://github.com/Siwhite11/weather-app"
+                  target="_blank"
+                >
+                  Stephanie White{" "}
+                </a>
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
