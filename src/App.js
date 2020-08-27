9755e9bfc9784c4da56812477f9504adf4a5b82f
Stephanie White <stephanieiwhite11@gmail.com>
2020-08-26 21:31:23 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,79 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 import Extras from "./Extras";
 
 export default function Container() {
-  return (
-    <div className="App">
-      <h1>New York</h1>
-      <h4>Mon August 3st 2020</h4>
-      <h2>
-        <span id="tempNow" />
-        75°F | °C
-      </h2>
-      <br />
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+  function handleResponse(response) {
+    setWeatherData({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      city: response.data.name,
+    });
+    setReady(true);
+  }
 
-      <div className="row">
-        <div class="col-6">
-          <span class="info" />
+  if (ready) {
+    return (
+      <div className="App">
+        <h1>{weatherData.city}}</h1>
+        <h4>Mon August 3rd 2020</h4>
+        <h2>
+          <span id="tempNow" />
+          {Math.round(weatherData.temperature)}°F | °C
+        </h2>
+        <br />
 
-          <form>
-            <input
-              className="search2"
-              id="location"
-              type="submit"
-              value="Current Location"
-            />
-          </form>
+        <div className="row">
+          <div class="col-6">
+            <span class="info" />
+
+            <form>
+              <input
+                className="search2"
+                id="location"
+                type="submit"
+                value="Current Location"
+              />
+            </form>
+
+            <ul>
+              <li>Description:{weatherData.description}</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {Math.round(weatherData.wind)}km/h</li>
+            </ul>
+
+            <br />
+          </div>
+          <div class="col">
+            <Extras />
+          </div>
 
           <br />
         </div>
         <div class="col">
-          <Extras />
+          <small className="me">
+            {" "}
+            Open Source code by{" "}
+            <a
+              href="https://github.com/Siwhite11/weather-react-app" // eslint-disable-next-line
+              target="_blank"
+            >
+              Stephanie White{" "}
+            </a>
+          </small>
         </div>
+      </div>
+    );
+  } else {
+    let apiKey = "619faf68e45e8f55d57c0c3b00b02041";
+    let city = "New York";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
 
-        <br />
-      </div>
-      <div class="col">
-        <small className="me">
-          {" "}
-          Open Source code by{" "}
-          <a
-            href="https://github.com/Siwhite11/weather-react-app" // eslint-disable-next-line
-            target="_blank"
-          >
-            Stephanie White{" "}
-          </a>
-        </small>
-      </div>
-    </div>
-  );
+    return "Loading...";
+  }
 }
