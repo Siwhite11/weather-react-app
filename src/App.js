c33eadc47eeefa3e2ff9b1c113ae66cb7b199fb1
Stephanie White <stephanieiwhite11@gmail.com>
2020-08-28 20:10:29 -0400

    updated description

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import axios from "axios";
 import "./App.css";
 import Extras from "./Extras";
 
-export default function Container() {
-  const [ready, setReady] = useState(false);
-  const [weatherData, setWeatherData] = useState(null);
+export default function Container(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     setWeatherData({
+      ready: true,
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       city: response.data.name,
+      description: response.data.weather[0].description,
+      iconUrl: response.data.weather[0].icon,
     });
-    setReady(true);
   }
 
-  if (ready) {
+  if (weatherData.ready) {
     return (
       <div className="App">
-        <h1>{weatherData.city}}</h1>
+        <h1>{weatherData.city}</h1>
         <h4>Mon August 3rd 2020</h4>
         <h2>
           <span id="tempNow" />
@@ -29,19 +30,9 @@ export default function Container() {
 
         <div className="row">
           <div class="col-6">
-            <span class="info" />
-
-            <form>
-              <input
-                className="search2"
-                id="location"
-                type="submit"
-                value="Current Location"
-              />
-            </form>
-
             <ul>
-              <li>Description:{weatherData.description}</li>
+              <li>{weatherData.iconUrl}</li>
+              <li>Description: {weatherData.description}</li>
               <li>Humidity: {weatherData.humidity}%</li>
               <li>Wind: {Math.round(weatherData.wind)}km/h</li>
             </ul>
@@ -70,8 +61,8 @@ export default function Container() {
     );
   } else {
     let apiKey = "619faf68e45e8f55d57c0c3b00b02041";
-    let city = "New York";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=imperial&appid=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
